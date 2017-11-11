94f44fe080639d3b02c5b10558ca8ef0b9264d87
StamplerJ <jann.luellmann@web.de>
2017-11-11 13:38:12 +0100

    Desktop setup

--- desktop/src/com/beemelon/physicsgame/desktop/DesktopLauncher.java
+++ desktop/src/com/beemelon/physicsgame/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import com.beemelon.physicsgame.PhysicsGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "PhysicsGame-demo";
+		config.width = 640;
+		config.height = 800;
 		new LwjglApplication(new PhysicsGame(), config);
 	}
 }
